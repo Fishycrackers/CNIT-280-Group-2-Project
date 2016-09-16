62f3e35b0526b7d46cb4c0b937d84e2315bb54ad
Fishycrackers <yyh12321@gmail.com>
2016-09-16 11:14:11 -0400

    File handling partial fix
    Fixed text fields parsing in PHP script, but not file parsing

--- pages/register/register.php
+++ pages/register/register.php
@@ -1,1 +1,23 @@
-<?php echo There is nothing here yet?>
+<?php
+
+	#Echo style sheet
+
+	$firstName="";
+	$lastName="";
+	$emailAddress="";
+	$manuscript="";
+	
+	$target_dir="../manuscripts";
+	
+	$firstName=!empty($_POST['first_name']) ? $_POST['first_name'] : '';
+	$lastName=!empty($_POST['last_name']) ? $_POST['last_name'] : '';
+	$emailAddress=!empty($_POST['email_address']) ? $_POST['email_address'] : '';
+	$manuscript=!empty($_FILES['manuscript']) ? $FILES['manuscript'] : '';
+
+	echo "Youre name is" . $firstName . "<br>";
+	echo "Your last name is " . $lastName . "<br>";
+	echo "Your email address is " . $emailAddress . "<br>";
+	echo "Your manuscript name is " . basename($_FILES["manuscript"]) . "<br>";
+	
+	
+?>
---
+++ test.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<?php echo $_GET["name"];?><br>
+</head>
+
+<body>
+HI <?php echo $_GET["name"]; ?><br>
+
+<?php echo "Hello this php script works"; ?>
+</body>
+</html>
