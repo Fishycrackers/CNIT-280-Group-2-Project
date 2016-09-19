ca99c2cc16f62d630d5ee1d641110be13bda83b1
Fishycrackers <yyh12321@gmail.com>
2016-09-19 14:30:48 -0400

    PHP Update
    Updates to some php stuff

--- pages/register/register.php
+++ pages/register/register.php
@@ -1,7 +1,7 @@
 <?php
 
 	#Echo style sheet
-
+	global $firstName, $lastName, $emailAddress, $manuscript;
 	$firstName="";
 	$lastName="";
 	$emailAddress="";
@@ -12,12 +12,42 @@
 	$firstName=!empty($_POST['first_name']) ? $_POST['first_name'] : '';
 	$lastName=!empty($_POST['last_name']) ? $_POST['last_name'] : '';
 	$emailAddress=!empty($_POST['email_address']) ? $_POST['email_address'] : '';
-	$manuscript=!empty($_FILES['manuscript']) ? $FILES['manuscript'] : '';
+	$manuscript=!empty($_FILES['manuscript']["name"]) ? $_FILES['manuscript'] : '';
 
-	echo "Youre name is" . $firstName . "<br>";
+	/*
+	echo "Youre name is " . $firstName . "<br>";
 	echo "Your last name is " . $lastName . "<br>";
 	echo "Your email address is " . $emailAddress . "<br>";
-	echo "Your manuscript name is " . basename($_FILES["manuscript"]) . "<br>";
 	
 	
+	if(isset($_FILES["manuscript"])){
+		echo "<br>" . implode($_FILES["manuscript"]) . " is set" ;
+		
+	} else {
+		echo "<br>" . implode($_FILES["manuscript"]) . " is NOT set";
+	}
+	
+	if(isset($_POST)){
+		echo "<br> POST set";
+		
+	} else {
+		echo "POST NOT set";
+		
+	}
+	*/
 ?>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../../standardDesign.css">
+	</head>
+	<body>
+		Thank you for submitting your registration info! <br>
+
+		Here is a report of your submitted data: <br>
+
+		First name: <?php$firstName?>
+
+
+	</body>
+
+</html>
---
+++ testingPHP.php
@@ -0,0 +1,41 @@
+<?php 
+#PHP Script 1
+
+
+
+
+
+
+
+
+
+
+?>
+
+<?php
+#PHP Script 2
+
+
+
+
+
+
+
+
+?>
+<html>
+	<head>
+		<title> PHP TESTING GROUNDS </title>
+		
+		<link rel="stylesheet" type="text/css" href="standardDesign.css"/>
+	</head>
+	<body>
+		<p>This is a PHP testing ground</p>
+		
+		<article>
+		Test Article
+		</article>
+		
+		
+	</body>
+</html>
